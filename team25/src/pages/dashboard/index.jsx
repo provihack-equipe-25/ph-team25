f1af45c14b75b70a110bb064a67225f954f69aef
LuizPombo <luizhfpr@gmail.com>
2022-05-01 19:04:00 -0300

    dashboard - iniciada

--- team25/src/pages/dashboard/index.jsx
+++ team25/src/pages/dashboard/index.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { useCompanies } from "../../provider/company";
+import { useWaste } from "../../provider/waste";
+
 const DashBoard = () => {
+  const [comp, setComp] = useState();
+  const [wst, setWst] = useState();
+
+  const { getCompanyId, company, companys, getCompanyWaste, getCompanyNeeds } =
+    useCompanies;
+    
+    const {}
+
   return <></>;
 };
 export default DashBoard;
