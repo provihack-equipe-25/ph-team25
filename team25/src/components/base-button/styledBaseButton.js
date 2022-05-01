79c9827e29fcc97945713e2a7ee2b36481b2d698
Leo Rigotto <leorigotto@me.com>
2022-05-01 14:50:29 -0300

    [feat] landing page completed

--- team25/src/components/base-button/styledBaseButton.js
+++ team25/src/components/base-button/styledBaseButton.js
@@ -7,6 +7,10 @@ export const ButtonContainer = styled.button`
   height: 60px;
   border: none;
   padding: 0 20px;
+  color: ${(props) => {
+    const setText = props.textColor
+    return props.textColor ? colors[setText] : "white"
+  }};
   background: ${(props) => {
     const setColor = props.color
     return props.color ? colors[setColor] : colors.main
@@ -15,7 +19,6 @@ export const ButtonContainer = styled.button`
   font-style: bold;
   font-weight: 900;
   font-size: 18px;
-  color: white;
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
   transition: 400ms ease;
   cursor: pointer;
