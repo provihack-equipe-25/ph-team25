4dfc524e12264d996a2f2f59963d4aa774a2f547
Leo Rigotto <leorigotto@me.com>
2022-05-01 09:06:58 -0300

    fix

--- team25/src/constants/theme.js
+++ team25/src/constants/theme.js
@@ -1,7 +1,10 @@
 export const colors = {
-  main: "#3A86FF",
-  mainlight: "#D1E9FF",
-  mainfade: "#EFFDFF",
-  secondary: "#4B4B4B",
-  background: "#A9A9A9",
+  main: "#00C3AA",
+  mainYellow: "#EBCC49",
+  mainWhite: "#FAFAFA",
+  secondary: "#4C56F5",
+  secondaryOrange: "#F5894C",
+  gray1: "#DBDBDB",
+  gray2: "#777777",
+  gray3: "373737"
 }
