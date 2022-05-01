94726ee5c0fb071b766e8309ad0485c9e83bb9a1
LuizPombo <luizhfpr@gmail.com>
2022-05-01 11:58:49 -0300

    page login - implemented

--- team25/src/App.js
+++ team25/src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom"
-import "./App.css"
-import Header from "./components/header/Header"
-import Router from "./router/Router"
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import Router from "./router/Router";
 
 function App() {
   return (
     <BrowserRouter>
-      <Router/>
+      <Router />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
