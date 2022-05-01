e521c46fd5d00f1633f0dd2342c16bfb2307c9a9
LuizPombo <luizhfpr@gmail.com>
2022-05-01 16:45:38 -0300

    login - correção

---
+++ team25/src/pages/register/index.jsx
@@ -0,0 +1,18 @@
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Container, Form, Input } from "./styles";
+import { useCompanies } from "../../provider/company";
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("@userToken");
+    if (token) {
+      navigate("/dashboard");
+    }
+  });
+};
